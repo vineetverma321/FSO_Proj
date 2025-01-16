513332ddf3cc8c49212d81a8faf3900950a8ebff
Vineet Verma <vineetverma321@gmail.com>
2025-01-16 16:38:42 +0530

    deploying

--- Node_Express/index.js
+++ Node_Express/index.js
@@ -1,10 +1,10 @@
 const http = require('http')
 const express = require('express')
 const app = express()
-// const cors = require('cors')
-// app.use(cors())
-
+const cors = require('cors')
 app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 
 let notes = [
   {
